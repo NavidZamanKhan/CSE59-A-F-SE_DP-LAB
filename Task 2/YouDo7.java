819984d6ec63887364a0dcdfd1139556db01699b
Navid Zaman Khan <navid166418@gmail.com>
2026-06-09 11:16:57 +0600

    Add Clickable interface and Button class implementation

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,16 @@
+interface clickable {
+    void onClick();
+}
 
+class Button implements Clickable {
+    @override
+    public void onClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Button button = new Button()
+    }
+}
