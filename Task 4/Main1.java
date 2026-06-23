e3b270d97bda959e22a597de7a25ef167e4c1543
Navid Zaman Khan <navid166418@gmail.com>
2026-06-23 09:52:15 +0600

    Implement shape interface and area calculations

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,47 @@
+interface Shape {
+    double calculateArea();
+}
 
+class Rectangle implements Shape {
+    public double length;
+    public double width;
+    
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Circle implements Shape {
+    public double radius;
+    
+    @Override
+    public double calculateArea() {
+        return Math.PI * (radius * radius);
+    }
+}
+
+class AreaCalculator {
+    public double calculateTotalArea(Shape[] shapes) {
+        double area = 0;
+        for (Shape shape : shapes) {
+            area += shape.calculateArea();
+        }
+        return area;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.length = 5;
+        r.width = 10;
+        
+        Circle c = new Circle();
+        c.radius = 7;
+        
+        Shape[] shapes = {r, c};
+        AreaCalculator calculator = new AreaCalculator();
+        System.out.println("Total Area: " + calculator.calculateTotalArea(shapes));
+    }
+}
