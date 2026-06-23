e8eaa7ce5c2d4e79900daf177f8a9c30ca2706d0
Navid Zaman Khan <navid166418@gmail.com>
2026-06-23 11:08:46 +0600

    Add interfaces and classes for printer functionality

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,45 @@
+interface Printer {
+    void printDocument();
+}
 
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+class BasicPrinter implements Printer {
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+class MultiFunctionalCopier implements Printer, Scanner, FaxMachine {
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument();
+        
+        MultiFunctionalCopier mfc = new MultiFunctionalCopier();
+        mfc.printDocument();
+        mfc.scanDocument();
+        mfc.faxDocument();
+    }
+}
