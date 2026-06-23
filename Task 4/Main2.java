312bf9fef0a496b4c14ff708c42421db03ba1f1b
Navid Zaman Khan <navid166418@gmail.com>
2026-06-23 09:52:37 +0600

    Implement TeamMember hierarchy with bonus calculation

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,33 @@
+class TeamMember {
+    protected String name;
+    public TeamMember(String name) {
+        this.name = name;
+    }
+}
 
+interface BonusEligible {
+    void calculateBonus();
+}
+
+class Employee extends TeamMember implements BonusEligible {
+    public Employee (String name) {
+        super(name);
+    }
+    
+    @Override
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus for " + name);
+    }
+}
+
+class Contractor extends TeamMember {
+    public Contractor (String name) { super(name);}
+}
+public class Main {
+    public static void main(String[] args) {
+        BonusEligible emp = new Employee("Alice");
+        emp.calculateBonus();
+        
+        Contractor con = new Contractor("Bob");
+    }
+}
